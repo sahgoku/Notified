1ef42542a1653519da867355c98744c1347f11a1
Jaures <ghislainsah@gmail.com>
2018-09-27 20:46:10 +0100

    Some modifications who will be deleted

--- app/src/main/java/com/jaures/notified/Main.java
+++ app/src/main/java/com/jaures/notified/Main.java
@@ -2,7 +2,12 @@ package com.jaures.notified;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class Main extends Activity {
 
@@ -11,5 +16,39 @@ public class Main extends Activity {
         super.onCreate(savedInstanceState);
         setTheme(R.style.Theme_AppCompat);
         setContentView(R.layout.lc);
+
+        private void getWebservice () {
+
+            OkHttpClient client;
+            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+            builder.connectTimeout(1, TimeUnit.MINUTES)
+                    .writeTimeout(1, TimeUnit.MINUTES)
+                    .readTimeout(1, TimeUnit.MINUTES);
+            client = builder.build();
+
+            Request request = new Request.Builder()
+                    .url(WebServiceBoutique.LISTE_PRODUITS + "?output_format=JSON&display=full")
+                    .get()
+                    .build();
+
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                    Log.e(TAG, e.getMessage());
+                }
+
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) {
+                    try {
+                        Log.e(TAG, response.body().string());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+        }
+
     }
 }
--- notifiedlib/src/main/java/com/jaures/ntifiedlib/Notified.kt
+++ notifiedlib/src/main/java/com/jaures/ntifiedlib/Notified.kt
@@ -2,9 +2,13 @@ package com.jaures.ntifiedlib
 
 import android.content.Context
 import android.content.res.TypedArray
+import android.graphics.Color
 import android.graphics.drawable.Drawable
+import android.support.v4.content.ContextCompat
+import android.support.v4.graphics.drawable.DrawableCompat
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
@@ -15,6 +19,7 @@ class Notified : FrameLayout {
     private var frameLayout: FrameLayout? = null
     private var m_number: TextView? = null
     private var a: TypedArray? = null
+    private var m_background: ImageView? = null
 
     init {
         initialise();
@@ -27,6 +32,9 @@ class Notified : FrameLayout {
 
         m_icon = view.findViewById(R.id.icon)
         m_number = view.findViewById(R.id.number)
+        m_background = view.findViewById(R.id.background)
+
+        m_icon!!.setVisibility(View.GONE);
     }
 
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
@@ -37,18 +45,38 @@ class Notified : FrameLayout {
                 0, 0)
 
         try {
-            // Count
-            val myString: String?;
-            myString = a.getString(R.styleable.notified_notified_count);
+            // Retrieve Styleable Count
+            val number: String?;
+            number = a.getString(R.styleable.notified_notified_count);
 
-            if (myString != null)
-                m_number!!.text = myString
+            if (number != null) {
 
-            //m_icon
+                // More or equal to value 100
+                if (Integer.valueOf(number) >= 100) {
+                    m_number!!.text = context.getString(R.string.ninetine_nine_plus);
+                } else
+                    m_number!!.text = number
+
+            }
+
+            // Retrieve Drawable Styleable Icon
             val myIcon: Drawable?;
             myIcon = a.getDrawable(R.styleable.notified_notified_icon)
-            if (myIcon != null)
+
+            if (myIcon != null) {
+                m_icon!!.setVisibility(View.VISIBLE);
                 m_icon!!.setImageDrawable(myIcon)
+            }
+
+
+            // Retrieve color
+            val color: Int?;
+            color = a.getColor(R.styleable.notified_notified_color, Color.RED);
+
+            if (color != null) {
+                DrawableCompat.setTint(m_background!!.getDrawable(), ContextCompat.getColor(context, color));
+                m_background
+            }
 
         } finally {
             a.recycle()
